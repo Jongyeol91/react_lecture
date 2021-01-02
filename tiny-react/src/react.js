f581717ec6052762d8ff10f57e6924bb3753e809
jongyeol park <pjr159@gmail.com>
2021-01-02 18:51:15 +0900

    [tiny-react] hooks principle

--- tiny-react/src/react.js
+++ tiny-react/src/react.js
@@ -1,3 +1,20 @@
+const hooks = [];
+let currentComponent = 0;// 어떤 함수가 호출되는지 useState와 createElement간의 정보공유
+
+export function useState(initValue) {
+    const position = currentComponent;
+    // 최초 호출이면
+    if (!hooks[position]) {
+        hooks[position] = initValue;
+    }
+    return [
+        hooks[position],
+        (nextValue) => {
+            hooks[position] = nextValue
+        }
+    ];
+}
+
 function renderRealDOM(vdom) {
     if (typeof vdom === 'string') {
        return document.createTextNode(vdom);
@@ -22,6 +39,7 @@ export const render = (() => {
         if (prevVdom === null) {
             prevVdom = nextVdom;
         }
+        // diff~
         container.appendChild(renderRealDOM(nextVdom))
     }
 })();
@@ -29,6 +47,9 @@ export const render = (() => {
 
 export function createElement(tagName, props, ...children) {
     if (typeof tagName === 'function') {
+        // 리액트 훅 공식문서에 훅을 반드시 함수형 컴포넌트 안에서먼, 조건적 호출에서는 쓰지 말아라
+        // 항상 동일하게 렌더링 되어야 currentComponent 순서가 맞기 때문
+        currentComponent++;
         return tagName.apply(null, [props, ...children])
     }
     return {tagName, props, children}
