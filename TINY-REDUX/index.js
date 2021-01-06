eefb337acdf23b71b820ef75b639d08858bfac1b
jongyeol park <pjr159@gmail.com>
2021-01-06 23:59:41 +0900

    redux updater

---
+++ TINY-REDUX/index.js
@@ -0,0 +1,9 @@
+import { createStore } from "./redux.js";
+
+function updater() {
+    
+}
+
+const store = createStore();
+
+console.log(1)
---
+++ TINY-REDUX/redux.js
@@ -0,0 +1,10 @@
+export function createStore() {
+    let state;
+
+    updater();
+
+    return {
+        
+    }
+}
+
